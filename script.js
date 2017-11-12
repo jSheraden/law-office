631ef3b6ba380d44151a7a955111faea43180728
joey <josasheraden@gmail.com>
2017-11-11 22:24:04 -0600

    Fixed JavaScript error, Google Maps integration now working.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var location = { 30.2364671, -97.7410959 };
+  var location = { lat: 30.2364671, lng: -97.7410959 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: location
