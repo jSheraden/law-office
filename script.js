2d6493cdda8de6e52f85699a32398c571b0002b3
jSheraden <josasheraden@gmail.com>
2017-11-11 21:50:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var location = {};
+  var location = { 30.2364671, -97.7410959 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: location
